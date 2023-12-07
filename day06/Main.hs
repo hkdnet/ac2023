f19766e9ab764fd51b1a47aa3b968f27d27bff80
hkdnet <satoko.itse@gmail.com>
2023-12-07 00:34:48 +0000

    day6-A

---
+++ day06/Main.hs
@@ -0,0 +1,93 @@
+module Main where
+
+import Data.List
+import Data.Map qualified as Map
+import Data.Maybe (fromJust)
+import System.Environment (getArgs)
+import Text.Parsec
+  ( Parsec,
+    char,
+    endBy1,
+    eof,
+    many,
+    oneOf,
+    parse,
+    string,
+  )
+import Text.Parsec.Expr
+import Text.Parsec.Language qualified as Lang
+import Text.Parsec.Token qualified as TT
+
+type Parser a = Parsec String () a
+
+type Input = ([Integer], [Integer])
+
+type Race = (Integer, Integer)
+
+pNatural :: Parser Integer
+pNatural = TT.natural Lang.haskell
+
+pTimeList :: Parser [Integer]
+pTimeList = do
+  string "Time:"
+  pSpaces
+  endBy1 pNatural (many (char ' '))
+
+pDistanceList :: Parser [Integer]
+pDistanceList = do
+  string "Distance:"
+  pSpaces
+  endBy1 pNatural pSpaces
+
+pSeeds :: Parser [Integer]
+pSeeds = do
+  _ <- string "seeds: "
+  endBy1 pNatural pSpaces
+
+pSpaces :: Parser [Char]
+pSpaces = many (char ' ')
+
+pInput :: Parser Input
+pInput = do
+  t <- pTimeList
+  d <- pDistanceList
+  return (t, d)
+
+toRaces :: ([Integer], [Integer]) -> [Race]
+toRaces (t, d) = zip t d
+
+-- solveA :: Input -> Integer
+solveA i = product $ map patternCount races
+  where
+    races = toRaces i
+
+patternCount :: Race -> Integer
+patternCount (t, d) = (half - minPush) * 2 + adjust
+  where
+    isEven = even t
+    adjust = if isEven then 1 else 0
+    half = if isEven then t `div` 2 else t `div` 2 + 1
+    minPush = fromJust $ minLongerThan d t
+
+minLongerThan :: Integer -> Integer -> Maybe Integer
+minLongerThan threshold t = find f [1 ..]
+  where
+    f p = p * (t - p) > threshold
+
+solveB :: Input -> Integer
+solveB _ = 1
+
+main :: IO ()
+main = do
+  args <- getArgs
+  str <- readFile ("day06/" ++ args !! 1)
+
+  -- either print print $ parse pInput "" test
+  f (head args) $ parse pInput "a" str
+  where
+    f ty = either err (ok ty)
+    ok ty i = do
+      if ty == "A"
+        then print $ solveA i
+        else print $ solveB i
+    err = print
