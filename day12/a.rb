89fe1b29fbb436039471d6d8d0f4e3374a73fff3
hkdnet <satoko.itse@gmail.com>
2023-12-15 17:51:06 +0900

    so so fast

--- day12/a.rb
+++ day12/a.rb
@@ -4,37 +4,20 @@ while (line = gets)
 end
 
 def possible?(s, nums, strict: false)
-  idx = 0
-  cnt = 0
-  s.chars.each do |c|
-    if c == '.'
-      if cnt != 0
-        if nums[idx] != cnt
-          return false
-        end
-
-        idx += 1
-        cnt = 0
-      end
-    else
-      cnt += 1
-    end
-  end
-
-  if cnt > 0
-    if cnt != nums[idx]
-      return false
-    end
-    idx += 1
+  cur = s.chars.chunk(&:itself).filter_map do |c, arr|
+    c == '#' ? arr.size : nil
   end
 
   if strict
-    if nums.size != idx
-      return false
-    end
-  end
+    cur == nums
+  else
+    return true if cur.size == 0
+    return false if cur.size > nums.size
 
-  true
+    abs = cur.zip(nums)
+    a, b = abs[-1]
+    abs[0..-2].all? {|a, b| a == b} && a <= b
+  end
 end
 
 ans = 0
@@ -56,40 +39,40 @@ lines.each do |line|
     a.each do |e|
       s = e + '.'
       # STDERR.puts "testing #{s}"
-      tmp << s # if possible?(s, nums)
+      tmp << s if possible?(s, nums)
       s = e + '#'
       # STDERR.puts "testing #{s}"
-      tmp << s # if possible?(s, nums)
+      tmp << s if possible?(s, nums)
     end
     # STDERR.puts tmp.inspect
 
     a = tmp
   end
 
-  # p a
+  p a
   a.select! { |e| possible?(e, nums, strict: true) }
   puts "#{line} -> #{a.size}"
-  # p a
-  # puts "---"
+  p a
+  puts "---"
   ans += a.size
 end
 
 puts "---"
 puts ans
 
-[
-  [".", [1], false, true],
-  ["#", [1], false, true],
-  ["##", [1], false, false],
-  ["#.", [1], false, true],
-  [".#", [1], false, true],
-  ["..", [1], false, true],
-  ["..", [1], true, false],
-].each do |s, nums, strict, expected|
-  actual = possible?(s, nums, strict:)
-  if actual == expected
-    puts "ok"
-  else
-    p "#{s} #{nums} #{strict} #{expected}"
-  end
-end
+# [
+#   [".", [1], false, true],
+#   ["#", [1], false, true],
+#   ["##", [1], false, false],
+#   ["#.", [1], false, true],
+#   [".#", [1], false, true],
+#   ["..", [1], false, true],
+#   ["..", [1], true, false],
+# ].each do |s, nums, strict, expected|
+#   actual = possible?(s, nums, strict:)
+#   if actual == expected
+#     puts "ok"
+#   else
+#     p "#{s} #{nums} #{strict} #{expected}"
+#   end
+# end
